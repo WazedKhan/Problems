81ce9f0047f0de0c3e3fbde8546433811a7e53b1
WazedKhan <51821594+WazedKhan@users.noreply.github.com>
2021-12-24 19:18:14 +0600

    If Brian did not overcharge Anna, print Bon Appetit

---
+++ Anna's_bill.py
@@ -0,0 +1,19 @@
+# values = list(map(int, input().rstrip().split()))
+
+n = 4
+k = 1
+
+bill = [3, 10, 2, 9]  #list(map(int, input().rstrip().split()))
+b = 7  #int(input().strip().split())
+
+def bonAppetit(bill, k, b):
+    del bill[k]
+    actual_charge = sum(bill)//2
+    over_charged = b - actual_charge
+
+    if over_charged != 0:
+        print(over_charged)
+    else:
+        print('Bon Appetit')
+
+bonAppetit(bill, k, b)
