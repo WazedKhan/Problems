1e55a25fcc9d2adfb029418df9b21ee87149e3cb
WazedKhan <51821594+WazedKhan@users.noreply.github.com>
2022-04-07 02:46:18 +0600

    swapcase then reverse

---
+++ reverse_sentance_swapcase.py
@@ -0,0 +1,9 @@
+from multiprocessing import Semaphore
+
+
+sentance = 'aWESOME is cODING'.swapcase()
+
+sentance = sentance.split()[::-1]
+reversed_sentence = ' '.join(sentance)
+
+print(reversed_sentence.strip())
