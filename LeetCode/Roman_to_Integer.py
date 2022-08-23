12125df2d87c5c6c1be18b1677a86594692b2c3a
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-08-23 18:17:12 +0600

    no pseu

--- LeetCode/Roman_to_Integer.py
+++ LeetCode/Roman_to_Integer.py
@@ -1,11 +1,3 @@
-"""
-[Pseudocode]
-step-1: make dict to store key as symbola value as the value of symbols
-step-2: take char from string
-step-3: find the char value in dict
-step-4: add and store value in var
-"""
-
 def romanToInt(s: str) -> int:
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     value = 0
