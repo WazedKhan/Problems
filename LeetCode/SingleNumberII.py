64ed24248e0e655b7f4c678eafa326ce70a9530c
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-08-25 19:19:57 +0600

    single Number Series

---
+++ LeetCode/SingleNumberII.py
@@ -0,0 +1,11 @@
+from collections import defaultdict
+
+def singleNumber(nums):
+    counter = defaultdict(int)
+    for letter in nums:
+        counter[letter] += 1
+    for key, value in counter.items():
+        if value == 1:
+            return key
+
+print(singleNumber(nums = [1,2,1,3,2,5]))
