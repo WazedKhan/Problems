48d29fff32c2024bc81b9732c41f60b959e5abd2
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-09-17 15:51:56 +0600

    55 ms, faster than 86.31%

--- LeetCode/ValidAnagram.py
+++ LeetCode/ValidAnagram.py
@@ -1,13 +1,20 @@
-def isAnagram(n, t):
-    for i in t:
-        if i not in n:
+def isAnagram(s, t):
+    if len(s) != len(t):
+        return False
+
+    hash_s = {}
+    hash_t = {}
+    for i in range(len(t)):
+        hash_s[s[i]] = 1 + hash_s.get(s[i], 0)
+        hash_t[t[i]] = 1 + hash_t.get(t[i], 0)
+
+    for i in hash_s:
+        if hash_s[i] != hash_t.get(i):
             return False
-        else:
-            n = n.replace(i, '', 1)
-    return len(n) <= 0, n
 
+    return True
 
-s = 'ab'
-t = 'as'
+s = 'cat'
+t = 'rat'
 
 print(isAnagram(s, t))
