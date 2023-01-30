a52726470388d996d233227f954c9d0f51bbd284
WazedKhan <wazedkhan119399@gmail.com>
2023-01-30 22:50:03 +0600

    Runtime: 38 ms | Beats: 41.59% Memory: 13.8 MB | Beats: 95.87%

---
+++ LeetCode/TribonacciNumber.py
@@ -0,0 +1,12 @@
+def solution(n):
+    t = [0, 1, 1]
+
+    if n < 3:
+        return t[n]
+
+    for i in range(3, n+1):
+        t[0], t[1], t[2] = t[1], t[2], sum(t)
+
+    return t[2]
+
+print(solution(4))
