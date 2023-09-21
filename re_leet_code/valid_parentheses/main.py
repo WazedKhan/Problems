649cd1ada89c02d31590ef1acafb177b73646fb9
WazedKhan <wazedkhan119399@gmail.com>
2023-09-22 00:21:37 +0600

    code cleaned up

--- re_leet_code/valid_parentheses/main.py
+++ re_leet_code/valid_parentheses/main.py
@@ -1,28 +1,14 @@
-from collections import Counter
-
-
-def get_reverse_parentheses(char):
-
-    if char == ")":
-        return "("
-    if char == "}":
-        return "{"
-    if char == "]":
-        return "["
-
-
 def isValid(s):
     parentheses_hash = {")": "(", "}": "{", "]": "["}
-
     stack = []
-    count = 0
+
     for i in s:
         if stack and i in parentheses_hash and stack[-1] == parentheses_hash[i]:
             stack.pop()
         else:
             stack.append(i)
 
-    return True if not stack else False
+    return not stack
 
 
 s = "()[(]{)}"
