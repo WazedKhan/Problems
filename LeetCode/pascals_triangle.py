d8c96fde4796bbcb20ebd23462260adfea32c098
WazedKhan <wazedkhan119399@gmail.com>
2024-07-02 07:49:03 +0600

    pascal's triangle problem I and II

---
+++ LeetCode/pascals_triangle.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        result = [[1]]
+        for i in range(numRows - 1):
+            tem_row = [0] + result[-1] + [0]
+            new_row = []
+            for j in range(len(result[-1]) + 1):
+                new_row.append(tem_row[j] + tem_row[j + 1])
+            result.append(new_row)
+        return result
+
+
+numRows = 5
+# Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+solution = Solution().generate(numRows)
+print(solution)
