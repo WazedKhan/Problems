a0a8007f994bf6d4c6d517247ff3d1f5d722212c
WazedKhan <wazedkhan119399@gmail.com>
2024-08-24 15:05:01 +0600

    included test case

--- tests/test_leetcode.py
+++ tests/test_leetcode.py
@@ -1,5 +1,9 @@
 import pytest
 
-def test_happy_number_202():
+
+@pytest.mark.parametrize("input_value, expected", [(19, True), (2, False)])
+def test_happy_number_202(input_value, expected):
     from LeetCode.happy_number_202 import Solution
-    solution =
+
+    solution = Solution().isHappy(input_value)
+    assert solution == expected
