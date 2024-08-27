ceebf11ca03445f865ee38584b7725350604410a
WazedKhan <wazedkhan119399@gmail.com>
2024-08-27 23:23:04 +0600

    unit test: renamed directory to for better import facilities

---
+++ tests/test_binary_tree.py
@@ -0,0 +1,9 @@
+from DSA.binary_tree.introduction.bfs_traversal import pre_order_dfs, Node
+
+
+def create_test_tree() -> Node:
+    pass
+
+# facilities
+def test_pre_order_traversal_dfs():
+    assert True == True
