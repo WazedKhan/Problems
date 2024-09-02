7965a7484d5afd7cac3cf51ebb9c77758e582b33
WazedKhan <wazedkhan119399@gmail.com>
2024-09-02 23:07:11 +0600

    solve: replace chalk leet code daily challenge

--- tests/test_leetcode.py
+++ tests/test_leetcode.py
@@ -7,3 +7,20 @@ def test_happy_number_202(input_value, expected):
 
     solution = Solution().isHappy(input_value)
     assert solution == expected
+
+
+@pytest.mark.parametrize(
+    "chalk, k, expected",
+    [
+        ([5, 1, 5], 22, 0),
+        ([3, 4, 1, 2], 25, 1),
+        ([1, 2, 3], 6, 0),
+        ([7, 5, 3, 1], 20, 0),
+    ],
+)
+def test_chalk_replacer(chalk, k, expected):
+    from LeetCode.replace_the_chalk import Solution
+
+    solution = Solution()
+    assert solution.chalk_replacer_brute_force(chalk, k) == expected
+    assert solution.chalkReplacer(chalk, k) == expected
