4838ecae5ce76bd9a1c4ab3856472531707ffbb1
WazedKhan <wazedkhan119399@gmail.com>
2024-09-13 08:41:34 +0600

    DFS: In Order B-Tree

--- DSA/binary_tree/introduction/bfs_traversal.py
+++ DSA/binary_tree/introduction/bfs_traversal.py
@@ -9,7 +9,9 @@ class BinaryTree:
     def __init__(self, head: Node) -> None:
         self.head = head
 
+    # Pre-order DFS: Root, Left, Right
     def travel_pre_order(self):
+        print("Pre-order DFS: Root, Left, Right: ", end=" ")
         if self.head is None:
             print("Provided Binary Tree is empty!")
 
@@ -21,6 +23,27 @@ class BinaryTree:
                 stack.append(node.right)
             if node.left:
                 stack.append(node.left)
+
+    # In-order DFS: Left, Root, Right
+    def in_order_dfs(self):
+        print("In-order DFS: Left, Root, Right: ", end=" ")
+        if self.head is None:
+            print("Provided Binary Tree is empty!")
+
+        stack = []
+        current = self.head
+
+        while current or stack:
+            # traverse left most element first
+            while current:
+                stack.append(current)
+                current = current.left
+            # check point where current none but stack is not
+            current = stack.pop()
+            print(current.value, end=" ")
+
+            # as the all the left has visited now we go right and start from current light's left
+            current = current.right
 
 
 # Creating nodes
@@ -38,6 +61,14 @@ node2.right = node5  # 5 is the right child of 2
 
 # Create BinaryTree with node1 as the head
 binary_tree = BinaryTree(node1)
-
+print(
+    """ Tree:
+                1
+            2       3
+        4       5
+    """
+)
 # Testing pre-order traversal
 binary_tree.travel_pre_order()
+print()
+binary_tree.in_order_dfs()
