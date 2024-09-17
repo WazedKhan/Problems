8b29834ccd25dc21ca8b090e3f956763321efd07
WazedKhan <wazedkhan119399@gmail.com>
2024-09-18 00:17:12 +0600

    counter: using counter modules fo python

--- LeetCode/uncommon_words_in_two_sen_884.py
+++ LeetCode/uncommon_words_in_two_sen_884.py
@@ -1,27 +1,18 @@
 # https://leetcode.com/problems/uncommon-words-from-two-sentences/
 
-from collections import defaultdict
+from collections import Counter
 from typing import List
 
 
 class Solution:
     def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
-        hash_map = defaultdict(int)
+
         result = list()
 
         full_sentence = s1.split(" ") + s2.split(" ")
-        for word in full_sentence:
-            hash_map[word] += 1
-
-        for key, value in hash_map.items():
-            if value == 1:
-                result.append(key)
+        counted_words = Counter(full_sentence)
+        for word in counted_words:
+            if counted_words[word] == 1 and word != "":
+                result.append(word)
 
         return result
-
-
-s1 = "this apple is sweet"
-s2 = "this apple is sour"
-
-solution = Solution().uncommonFromSentences(s1, s2)
-print(solution)
