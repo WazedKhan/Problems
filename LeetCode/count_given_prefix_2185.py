fc5cea7f5269d12733427018cc6913fd4d104588
Abdul Wajed Khan <51821594+WazedKhan@users.noreply.github.com>
2025-01-09 17:28:02 +0600

    create count_given_prefix_2185

---
+++ LeetCode/count_given_prefix_2185.py
@@ -0,0 +1,7 @@
+class Solution:
+    def prefixCount(self, words: List[str], pref: str) -> int:
+        result = 0
+        for word in words:
+            if word.startswith(pref):
+                result += 1
+        return result
