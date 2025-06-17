2c4fbf3abda7902de6fc893d2eeed40357fbd1be
WazedKhan <wazedkhan119399@gmail.com>
2025-06-17 20:36:42 +0600

    658: find k closest element

--- Goal/code/week_02.py
+++ Goal/code/week_02.py
@@ -25,9 +25,26 @@ class Solution:
 
         return []
 
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        left, right = 0, len(arr) - k
+
+        while left < right:
+            mid = (left + right) // 2
+            if x - arr[mid] > arr[mid + k] - x:
+                left = mid + 1
+            else:
+                right = mid
+
+        return arr[left : left + k]
+
 
 solution = Solution()
 numbers = [2, 3, 4]
 target = 6
 
+arr = [1, 2, 3, 4, 5]
+k = 4
+x = 3
+
 print("Two Sum ll Result:", solution.twoSum(numbers, target))
+print("Find Closest Elements Result:", solution.findClosestElements(arr, k, x))
