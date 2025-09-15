ebd67b48ee38fca5634679933ccb6e45502ae239
Wazed Khan <wazedkhan119399@gmail.com>
2025-09-15 06:13:48 +0600

    upt: two sum with go

--- Go/two_sum.go
+++ Go/two_sum.go
@@ -1,16 +1,13 @@
 package main
 
 func TwoSum(nums []int, target int) []int {
-	visited_values := make(map[int]int)
-
+	seen := make(map[int]int)
 	for index, value := range nums {
-		required_value := target - value
-
-		if required_value_index, exists := visited_values[required_value]; exists{
-			return []int{index, required_value_index}
+		required := target - value
+		if idx, ok := seen[required]; ok {
+			return []int{index, idx}
 		}
-		// required value not in visited value then insert it
-		visited_values[value] = index
+		seen[value] = index
 	}
 	return nil
 }
