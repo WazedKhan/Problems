45bedcec4771e2809bc7791f50574129f743e164
wazedkhan <wazedkhan119399@gmail.com>
2026-01-02 09:56:07 +0600

    leetcode 961: N-Repeated Element in Size 2N Array

---
+++ LeetCode/easy/n_repeated_element_961.py
@@ -0,0 +1,12 @@
+# 961. N-Repeated Element in Size 2N Array
+# Link: https://leetcode.com/problems/n-repeated-element-in-size-2n-array/description/?envType=daily-question&envId=2026-01-02
+from typing import List
+
+class Solution:
+    def repeatedNTimes(self, nums: List[int]) -> int:
+        seen_hash = {}
+
+        for num in nums:
+            if num in seen_hash:
+                return num
+            seen_hash[num] = 1
