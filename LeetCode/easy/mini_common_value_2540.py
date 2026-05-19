c5a455fa4bfe461f412c02118426b525d52b7e29
wazedkhan <wazedkhan119399@gmail.com>
2026-05-19 21:23:06 +0600

    feat: minimum common value solved with two pointer

--- LeetCode/easy/mini_common_value_2540.py
+++ LeetCode/easy/mini_common_value_2540.py
@@ -2,9 +2,15 @@ from typing import List
 
 class Solution:
     def getCommon(self, nums1: List[int], nums2:List[int]) -> int:
-        seen = set(nums1)
-        for num in nums2:
-            if num in seen:
-                return num
+        point_one = 0
+        point_two = 0
+
+        while len(nums1) > point_one and len(nums2) > point_two:
+            if nums2[point_two] == nums1[point_one]:
+                return nums1[point_one]
+            elif nums1[point_one] < nums2[point_two]:
+                point_one += 1
+            else:
+                point_two += 1
 
         return -1
