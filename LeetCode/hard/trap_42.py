032151b192ffb18611cc51fe3e6b162896db496e
wazedkhan <wazedkhan119399@gmail.com>
2026-06-21 10:11:49 +0600

    LeetCode #42: Trapping Rain Water - (Two Pointer)

--- LeetCode/hard/trap_42.py
+++ LeetCode/hard/trap_42.py
@@ -6,20 +6,18 @@ class Solution:
         if not height:
             return 0
 
-        h_len = len(height)
-        max_left = [None] * h_len
-        max_right = [None] * h_len
-
-        max_left[0] = 0
-        for i in range(1, h_len):
-            max_left[i] = max(max_left[i - 1], height[i - 1])
-
-        max_right[-1] = 0
-        for i in range(h_len - 2, -1, -1):
-            max_right[i] = max(max_right[i + 1], height[i + 1])
-
+        left, right = 0, len(height) - 1
+        left_max, right_max = height[left], height[right]
         res = 0
-        for i in range(h_len):
-            trapped = min(max_left[i], max_right[i]) - height[i]
-            res += max(0, trapped)
+
+        while left < right:
+            if left_max < right_max:
+                left += 1
+                left_max = max(left_max, height[left])
+                res += left_max - height[left]
+            else:
+                right -= 1
+                right_max = max(right_max, height[right])
+                res += right_max - height[right]
+
         return res
