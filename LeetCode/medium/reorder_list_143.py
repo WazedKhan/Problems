e0c55fc259bf76c34a3a65fa65c8b1dd40131e80
wazedkhan <wazedkhan119399@gmail.com>
2026-07-02 09:09:14 +0600

    LeetCode #143: Reorder List

---
+++ LeetCode/medium/reorder_list_143.py
@@ -0,0 +1,41 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+
+    def print_linked_list(self, head):
+        current = head
+        while current is not None:
+            print(current.val, end="")
+            current = current.next
+        print()
+
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        slow, fast = head, head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        half = slow.next
+        slow.next = None
+
+        prev = None
+        while half:
+            next_node = half.next
+            half.next = prev
+            prev = half
+            half = next_node
+
+        while head and prev:
+            next_head = head.next
+            head.next = prev
+            next_prev = prev.next
+            prev.next = next_head
+            head = next_head
+            prev = next_prev
