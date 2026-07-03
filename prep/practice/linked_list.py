c342d898494d8a070a620951faa638dc194a75f0
wazedkhan <wazedkhan119399@gmail.com>
2026-07-03 09:26:05 +0600

    LeetCode #21: Merge Two Sorted Lists

--- prep/practice/linked_list.py
+++ prep/practice/linked_list.py
@@ -26,3 +26,23 @@ class Solution:
             prev = current
             current = next_node
         return prev
+
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        tail = dummy
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
+
+        if list1:
+            tail.next = list1
+        else:
+            tail.next = list2
+
+        return dummy.next
