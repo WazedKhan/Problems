01dc7e038f5641f36749eab583ef495d981e5a3c
wazedkhan <wazedkhan119399@gmail.com>
2026-07-03 16:45:42 +0600

    LeetCode-141: Linked List Cycle(set)

--- prep/practice/linked_list.py
+++ prep/practice/linked_list.py
@@ -46,3 +46,12 @@ class Solution:
             tail.next = list2
 
         return dummy.next
+
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        seen = set()
+        while head:
+            if head in seen:
+                return True
+            seen.add(head)
+            head = head.next
+        return False
